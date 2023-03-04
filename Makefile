294d9837f57400a6d9068abb432c9740af6936ba
Spansos <stan.spaans@student.hu.nl>
2023-03-04 14:23:19 +0100

    setup

---
+++ Makefile
@@ -0,0 +1,13 @@
+CC=g++
+CFLAGS=-g -Wall
+SRCFILES = $(wildcard src/*.cpp)
+OBJFILES = $(patsubst src/%.cpp,obj/%.o,$(SRCFILES))
+
+
+all: bin/main
+
+obj/%.o: src/%.cpp
+	$(CC) $(CFLAGS) -c $^ -Iinclude -o $@
+
+bin/main: $(OBJFILES)
+	$(CC) $(CFLAGS) -static -o $@ $^ $(wildcard lib/*)
---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+class Entity {
+};
+
+
+int main() {
+  const int gameWidth = 800;
+  const int gameHeight = 600;
+  sf::RenderWindow window(sf::VideoMode(gameWidth, gameHeight, 32), "Dark Forest", sf::Style::Titlebar | sf::Style::Close);
+  window.setFramerateLimit(30);
+
+  // sf::Texture texture;
+  // if (!texture.loadFromFile("")) {
+  //   return EXIT_FAILURE;
+  // };
+
+  sf::Clock clock;
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if ( (event.type == sf::Event::Closed) || ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape)) ) {
+        window.close();
+        break;
+      }
+    }
+    window.clear(sf::Color(86, 100, 128));
+    window.display();
+  }
+
+  return EXIT_SUCCESS;
+}
